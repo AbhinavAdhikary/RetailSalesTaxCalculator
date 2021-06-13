99fcc537ed791ba9ffbd438b08e4e5836b114e25
AbhinavAdhikary <abhinav.adhikary@gmail.com>
2021-06-13 19:45:18 -0400

    Upload

---
+++ RetailSalesTaxCalculator/Controllers/CalculatorController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RetailSalesTaxCalculator.Controllers
+{
+    public class CalculatorController : ApiController
+    {
+        [HttpGet]
+        public double[] Calculate(double amount, int zip)
+        {
+			double[] result = new double[2];
+			double taxAmount = 0;
+			double totalAmount = 0;
+
+			// Using 2 NC county zip codes for testing
+			if (zip == 27701)
+			{
+				taxAmount = (7.5 / 100) * amount;
+				totalAmount = amount + taxAmount;
+			}
+			else if (zip == 27401)
+			{
+				taxAmount = (6.75 / 100) * amount;
+				totalAmount = amount + taxAmount;
+			}
+			else
+			{
+				taxAmount = 0;
+			}
+
+			result[0] = taxAmount;
+			result[1] = totalAmount;
+
+			return result;
+		}
+    }
+}
---
+++ RetailSalesTaxCalculator/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RetailSalesTaxCalculator.WebApiApplication" Language="C#" %>
